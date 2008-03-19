d4cec871317cf17d85e9d868616137662661e61f
Christian Ritz <ritz@life.ku.dk>
2008-03-19 00:00:00 +0000

    version 1.0

---
+++ R/onAttach.R
@@ -0,0 +1,9 @@
+.onAttach <- function(libname, pkgname)
+{
+    cat("\n")
+    cat("'nlrwr' has been loaded.\n\n")
+    cat("Please cite R and 'nlrwr' if used for a publication,\n")
+    cat("for references type 'citation()' and 'citation('nlrwr')'.\n\n")
+    
+    library(drc)
+}
